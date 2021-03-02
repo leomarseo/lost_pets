c98bef4073bb7e9d824d8fe6c720fd31dacaa178
Leonardo Marsigli <leonardomarsigli@gmail.com>
2021-03-02 18:31:05 +0100

    refactored controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,11 +1,10 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
   def index
     @pets = Pet.all
   end
 
-  def show
-    @pet = Pet.find(params[:id])
-  end
+  def show; end
 
   def new
     @pet = Pet.new
@@ -16,19 +15,15 @@ class PetsController < ApplicationController
     redirect_to pet_path(@new_pet)
   end
 
-  def edit
-    @pet = Pet.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @pet = Pet.find(params[:id])
     @pet.update(pet_params)
 
     redirect_to pet_path(@pet)
   end
 
   def destroy
-    @pet = Pet.find(params[:id])
     @pet.destroy
 
     redirect_to pets_path
@@ -37,6 +32,10 @@ class PetsController < ApplicationController
   private
 
   def pet_params
-    params.require(:pet).permit(:name, :address, :species)
+    params.require(:pet).permit(:name, :address, :species, :found_on)
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:id])
   end
 end
